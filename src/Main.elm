90becc6708ed21b0767d6502661351d1f92a417a
mediocreHacker <mediocreHacker@protonmail.com>
2017-04-06 20:46:13 +0200

    Elm Architecture

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,51 @@
-import Html
+module Main exposing (..)
 
+import Html exposing (Html, program, div)
+
+
+-- Model
+
+
+type alias Model =
+    { pol : String
+    , pod : String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( { pol = "", pod = "" }, Cmd.none )
+
+
+
+-- Update
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div [] []
+
+
+main : Program Never Model Msg
 main =
-  Html.text "Shipper Savers / Find best tariffs and rates for all ocean shipping lines"
+    program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = (\x -> Sub.none)
+        }
